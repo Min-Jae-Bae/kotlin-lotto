96a8c298bbcfe7e71fdcb729559e33b9f91bfea3
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-05 16:24:03 +0900

    [Fix] (InputView.kt) 입력 길이 오류 해결

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -13,15 +13,14 @@ class InputView {
     // 당첨 번호 입력
     fun createWinningNumber(): List<Int> {
         println("당첨 번호를 입력해 주세요.")
-        val winningNumber = readLine()!!.replace(",", "")
-        return winningNumber.map { number -> number.code }
+        val winningNumber = readLine()!!.split(",")
+        return winningNumber.map { number -> number.toInt() }
     }
 
     // 보너스 번호 입력
     fun createBonusNumber(): Int {
         println("보너스 번호를 입력해 주세요.")
         val bonusNumber = readLine()!!
-        require(bonusNumber.length == 1)
         return bonusNumber.toInt()
     }
 }
