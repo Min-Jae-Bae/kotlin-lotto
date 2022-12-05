0189c03904d358be56c129f54905cd03e36c92e7
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-05 16:23:06 +0900

    [Refactor] (Constant.kt, Rank.kt) enum 클래스 제작 및 상수화

---
+++ src/main/kotlin/lotto/util/Rank.kt
@@ -0,0 +1,9 @@
+package lotto.util
+
+enum class Rank(val numberMatches: Int, val money: Int, val message: String) {
+    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - %d개"),
+    SECOND(7, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
+    THIRD(5, 1500000, "5개 일치 (1,500,000원) - %d개"),
+    FOURTH(4, 50000, "4개 일치 (50,000원) - %d개"),
+    FIFTH(3, 5000, "3개 일치 (5,000원) - %d개"),
+}
