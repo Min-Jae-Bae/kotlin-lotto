d02657b36bc06492944495eefeb5e6fb1c95a771
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-07 00:44:41 +0900

    [Feat] (OutputView.kt) 로또 목록, 통계 결과 출력 기능

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -1,35 +1,24 @@
 package lotto.view
 
-import camp.nextstep.edu.missionutils.Randoms
 import lotto.util.PRINT_PURCHASE_LOTTERIES_MESSAGE
-import lotto.util.RATE_RETURN_MESSAGE
+import lotto.util.RETURN_TOTAL_MESSAGE
 import lotto.util.Rank
 
 class OutputView {
 
-    // 구매 출력
-    fun printPurchase(purchaseMoney: Int) {
-        val lotteriesCount = purchaseMoney / 1000
-        println(PRINT_PURCHASE_LOTTERIES_MESSAGE.format(lotteriesCount))
-        printLotteries(lotteriesCount)
-    }
-
-    fun printLotteries(lotteriesCount: Int) {
-        repeat(lotteriesCount) {
-            val number = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-            println(number.sorted())
-        }
+    fun printLotto(lotto: HashMap<Int, List<Int>>) {
+        println(PRINT_PURCHASE_LOTTERIES_MESSAGE.format(lotto.size))
+        lotto.forEach { (_, lottoList) -> println(lottoList) }
     }
 
     // 결과 출력
-    fun printResult() {
+    fun printResult(winningList: HashMap<Rank, Int>, returnTotal: Double) {
         println("당첨 통계\n---")
-        println(Rank.FIFTH.message.format(1))
-        println(Rank.FOURTH.message.format(0))
-        println(Rank.THIRD.message.format(0))
-        println(Rank.SECOND.message.format(0))
-        println(Rank.FIRST.message.format(0))
-        println(RATE_RETURN_MESSAGE.format(62.5))
-
+        println(Rank.FIFTH.message.format(winningList[Rank.FIFTH]))
+        println(Rank.FOURTH.message.format(winningList[Rank.FOURTH]))
+        println(Rank.THIRD.message.format(winningList[Rank.THIRD]))
+        println(Rank.SECOND.message.format(winningList[Rank.SECOND]))
+        println(Rank.FIRST.message.format(winningList[Rank.FIRST]))
+        println(RETURN_TOTAL_MESSAGE.format(returnTotal))
     }
 }
