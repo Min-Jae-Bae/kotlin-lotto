310759afb70669cd4bd32ba7fcc87e09b8b2ea6c
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-07 00:46:21 +0900

    [Refactor] (Rank.kt, Contstant.kt) Rank 매치 갯수, 출력 메시지, 상금 상수화

--- src/main/kotlin/lotto/util/Constant.kt
+++ src/main/kotlin/lotto/util/Constant.kt
@@ -1,4 +1,4 @@
 package lotto.util
 
 const val PRINT_PURCHASE_LOTTERIES_MESSAGE = "%d개를 구매했습니다."
-const val RATE_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다."
+const val RETURN_TOTAL_MESSAGE = "총 수익률은 %.1f%%입니다."
--- src/main/kotlin/lotto/util/Rank.kt
+++ src/main/kotlin/lotto/util/Rank.kt
@@ -1,6 +1,6 @@
 package lotto.util
 
-enum class Rank(val numberMatches: Int, val money: Int, val message: String) {
+enum class Rank(val matchCount: Int, val money: Int, val message: String) {
     FIRST(6, 2000000000, "6개 일치 (2,000,000,000원) - %d개"),
     SECOND(7, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개"),
     THIRD(5, 1500000, "5개 일치 (1,500,000원) - %d개"),
