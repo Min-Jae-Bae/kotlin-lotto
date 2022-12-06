f63bfcc8482d28d58ff349aad7f519e1af109794
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-07 00:49:44 +0900

    [Feat] (LottoGame.kt) Game Controller 기능

--- src/main/kotlin/lotto/game/LottoGame.kt
+++ src/main/kotlin/lotto/game/LottoGame.kt
@@ -1,7 +1,9 @@
 package lotto.game
 
+import camp.nextstep.edu.missionutils.Randoms
 import lotto.game.service.Game
 import lotto.model.Lotto
+import lotto.model.ReturnTotalProcessor
 import lotto.view.InputView
 import lotto.view.OutputView
 
@@ -16,28 +18,18 @@ class LottoGame(
     }
 
     override fun process() {
-        /*
-        * 구입 금액 입력
-        * 로또 갯수와 로또를 출력
-        *
-        * 당첨 번호 입력
-        * 보너스 번호 입력
-        *
-        * 당첨 통계와 수익률 출력
-        * */
         try {
-            val purchaseMoney = inputView.createMoney()
-            outputView.printPurchase(purchaseMoney)
+            val money = inputView.createMoney()
+            outputView.printLotto(ReturnTotalProcessor.createLotteries(money))
             val winningNumber = inputView.createWinningNumber()
             val bonusNumber = inputView.createBonusNumber()
-
-            println(Lotto(winningNumber).compareNumber(bonusNumber))
-
-            outputView.printResult()
+            outputView.printResult(
+                ReturnTotalProcessor.createMatchCountList(winningNumber = winningNumber,
+                    bonusNumber = bonusNumber),
+                ReturnTotalProcessor.createTotalReturn(money)
+            )
         } catch (e: NumberFormatException) {
             println("[ERROR]")
         }
-
-
     }
 }
--- src/main/kotlin/lotto/model/Lotto.kt
+++ src/main/kotlin/lotto/model/Lotto.kt
@@ -1,38 +1,7 @@
 package lotto.model
 
-import camp.nextstep.edu.missionutils.Randoms
-import lotto.util.Rank
-
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6)
-    }
-
-    private val rankHashMap: MutableMap<Rank, Int> = mutableMapOf()
-
-    // 번호 비교
-    fun compareNumber(bonusNumber: Int) {
-        /*5등 조건 - FIFTH
-        * 4등 조건 - FOURTH
-        * 3등 조건 - THIRD
-        * 2등 조건 - SECOND
-        * 1등 조건 - FIRST
-        * */
-        var matchCount = 0
-        val userLottery = Randoms.pickUniqueNumbersInRange(1, 45, 6)
-        userLottery.forEach { userNumber ->
-            // 보너스 제외
-            if (numbers.contains(userNumber)) matchCount++
-            // 보너스 포함
-            if (!numbers.contains(userNumber) && userNumber == bonusNumber) matchCount += 2
-        }
-
-        when (matchCount) {
-            Rank.FIFTH.numberMatches -> rankHashMap[Rank.FIFTH] = Rank.FIFTH.money
-            Rank.FOURTH.numberMatches -> rankHashMap[Rank.FOURTH] = Rank.FOURTH.money
-            Rank.THIRD.numberMatches -> rankHashMap[Rank.THIRD] = Rank.THIRD.money
-            Rank.SECOND.numberMatches -> rankHashMap[Rank.SECOND] = Rank.SECOND.money
-            Rank.FIRST.numberMatches -> rankHashMap[Rank.FIRST] = Rank.FIRST.money
-        }
     }
 }
